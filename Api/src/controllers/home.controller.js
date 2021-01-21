bb58aa3069fbb0945d3e3026fbf8fbabcb80639a
jhosep98 <jhosep149@hotmail.com>
2021-01-21 12:55:03 -0300

    adding new SQL query
    adding all the data in the amount column to determine the balance.

--- Api/src/controllers/home.controller.js
+++ Api/src/controllers/home.controller.js
@@ -1,8 +1,11 @@
 import { pool } from "../config/connectionDatabase";
 
 export const getLastOperations = async (req, res) => {
-  const response = await pool.query(
-    "SELECT * FROM operations ORDER BY operation_id DESC LIMIT 10"
-  );
-  res.status(200).json(response.rows);
+  const response = await Promise.all([
+    pool.query("SELECT SUM(amount) FROM operations"),
+    pool.query("SELECT * FROM operations ORDER BY operation_id DESC LIMIT 10"),
+  ]);
+  console.log(response);
+  const resp = response.map((n) => n.rows);
+  res.status(200).json(resp);
 };
